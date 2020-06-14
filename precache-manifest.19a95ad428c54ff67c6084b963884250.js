dc19172e5b23ed0a08078afd8752ca826816d43d
neelesh7singh <neeleshsingh767@gmail.com>
2020-06-15 01:41:52 +0530

    Updates

---
+++ precache-manifest.19a95ad428c54ff67c6084b963884250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c39076509e91fb8742fb47425f7f5e",
+    "url": "/react-page-transition/index.html"
+  },
+  {
+    "revision": "ac13f61dfc413621b2e7",
+    "url": "/react-page-transition/static/css/main.d5389ca0.chunk.css"
+  },
+  {
+    "revision": "c97139d6e2d757c8044f",
+    "url": "/react-page-transition/static/js/2.1587557d.chunk.js"
+  },
+  {
+    "revision": "8f1544b07245491ea591f336c2dcc0b8",
+    "url": "/react-page-transition/static/js/2.1587557d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac13f61dfc413621b2e7",
+    "url": "/react-page-transition/static/js/main.d30f5a75.chunk.js"
+  },
+  {
+    "revision": "5f4dc9ebacb5683fbe5f",
+    "url": "/react-page-transition/static/js/runtime-main.ad1d3137.js"
+  }
+]);
